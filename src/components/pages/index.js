a272dc8c5c7d80f3342abf4b0773700b1bdc5381
coseo12 <momo_12@naver.com>
2020-11-05 21:38:02 +0900

    폴더 구조 추가 수정

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,5 +1,6 @@
+import App from '@/components/pages/App.vue';
 import DashboardPage from '@/components/pages/DashboardPage.vue';
 import MoviePage from '@/components/pages/MoviePage.vue';
 import TvPage from '@/components/pages/TvPage.vue';
 
-export { DashboardPage, MoviePage, TvPage };
+export { App, DashboardPage, MoviePage, TvPage };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from '@/components/App.vue';
+import { App } from '@/components/pages';
 import router from '@/router';
 
 createApp(App)
