88fe816a88e075dfdadf8a463300b61531b90eb7
JoyNaraShin <joyshin93@gmail.com>
2020-11-09 17:06:50 +0900

    commit dashboard to merge

--- src/components/pages/DashboardPage.vue
+++ src/components/pages/DashboardPage.vue
@@ -3,9 +3,16 @@
     Dashboard
   </div>
 </template>
-
 <script>
+import { watchEffect } from 'vue';
+import useFetchList from '@/composables/useFetchList';
 export default {
   name: 'DashboardPage',
+  setup() {
+    const { movieList } = useFetchList();
+    watchEffect(() => {
+      movieList.value.forEach(i => console.log(i));
+    });
+  },
 };
 </script>
