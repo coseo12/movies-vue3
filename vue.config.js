efacfbd8dca54189e89c3741f334270d009f5f19
coseo12 <momo_12@naver.com>
2021-02-27 16:19:33 +0900

    publicPath modify

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-nomflix/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/movies-vue3/' : '/',
   css: {
     loaderOptions: {
       scss: {
